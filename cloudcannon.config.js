21850e5c2ca6813e27dd2cfe6efbac5c0c345220
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-11-25 12:54:20 +1300

    Add structure-building javascript config file

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,31 +1,35 @@
+const filters = require("./data/filters.json");
+
 module.exports = {
     _structures: {
         filters: {
-            values: [
-                {
-                    label: "Animal Filter",
+            // Create a structure value for each filter section in our data file
+            values: filters.available_filters.map(({ filter_name, available_filter_values }) => {
+                return {
+                    label: `Filter Section: ${filter_name}`,
+                    value: {
+                        // Show the filter section name (we disable editing this using _inputs below)
+                        filter_section: filter_name,
+                        selected_value: null
+                    },
+                    // Identify this structure based on the filter_section value
                     id_key: "filter_section",
+                    subtext_key: "selected_value",
                     _inputs: {
                         filter_section: {
                             type: "disabled"
                         },
+                        // Let this type of filter section pick from the filter values
+                        // attached to it in the data file
                         selected_value: {
                             type: "select",
                             options: {
-                                values: [
-                                    "Moth",
-                                    "Grub"
-                                ]
+                                values: available_filter_values
                             }
                         }
                     },
-                    subtext_key: "selected_value",
-                    value: {
-                        filter_section: "Animal",
-                        selected_value: null
-                    }
                 }
-            ]
+            })
         }
     }
 }
