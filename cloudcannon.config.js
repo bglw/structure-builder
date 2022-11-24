d7f8db1cbca2bb47b17e38f175abf9942d70c380
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-11-25 11:54:31 +1300

    Hardcoded test

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-
+    _structures: {
+        filters: {
+            values: [
+                {
+                    label: "Animal Filter",
+                    id_key: "filter_section",
+                    _inputs: {
+                        filter_section: {
+                            type: "disabled"
+                        },
+                        selected_value: {
+                            type: "select",
+                            options: {
+                                values: [
+                                    "Moth",
+                                    "Grub"
+                                ]
+                            }
+                        }
+                    },
+                    subtext_key: "selected_value",
+                    value: {
+                        filter_section: "Animal",
+                        selected_value: null
+                    }
+                }
+            ]
+        }
+    }
 }
